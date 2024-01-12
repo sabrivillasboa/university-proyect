c23a42e33043762943abe6229f28e46ef38f5066
Sabrina <sabrinavillasboa@gmail.com>
2024-01-12 01:52:04 -0300

    cambios de estilos header y header top

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const navTop= document.querySelector(".top-header");
-const icon= document.querySelector(".top-header__icon");
+const icons= document.querySelectorAll(".top-header__icon");
+const navbar= document.querySelector(".nav-switch");
+const btn= document.querySelector(".navbar-toggler")
 
 window.addEventListener("scroll", ()=>{
     if (window.scrollY > 0){
         navTop.classList.add("switch-color");
         navTop.classList.remove("top-header");
-        icon.classList.add("switch-icon");
-        icon.classList.remove("top-header__icon");
+        
+        icons.forEach(icon => {
+            icon.classList.add("switch-icon");
+            icon.classList.remove("top-header__icon");
+          });
+
+        navbar.classList.add("nav-switch");
+        //navbar.classList.remove("navbar-dark");
         
     } else {
-      navTop.classList.remove("switch-color");
-      navTop.classList.add("top-header");
+        navTop.classList.remove("switch-color");
+        navTop.classList.add("top-header");
+        
+        icons.forEach(icon => {
+            icon.classList.remove("switch-icon");
+            icon.classList.add("top-header__icon");
+          });
+
+        navbar.classList.add("navbar-dark");
+        navbar.classList.remove("nav-switch");
     }
 })
 
-
+btn.addEventListener("click", ()=>{
+    navbar.classList.add("nav-switch");
+})
 
 
 
